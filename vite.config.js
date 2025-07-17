7a7999d600a85f9df07f3e44529a2d0aa873bb98
Fabian Guerrero <guerrerofabian1985@gmail.com>
2025-07-17 13:05:12 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          animation: ["gsap"],
+        },
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
