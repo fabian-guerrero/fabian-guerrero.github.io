1846d2cf5326b490291793037d9ce1493487b70a
Fabian Guerrero <guerrerofabian1985@gmail.com>
2025-07-17 13:36:45 +0200

    Silence sass deprecation warning on build

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
+        api: "modern",
+        silenceDeprecations: ["legacy-js-api"],
       },
     },
   },
