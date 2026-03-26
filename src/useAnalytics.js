ae5b97bc3b8c760fc47dc546d50d3d0054b266f4
Fabian Guerrero <guerrerofabian1985@gmail.com>
2026-03-26 22:08:19 +0100

    Defers GA config until script loads

--- src/useAnalytics.js
+++ src/useAnalytics.js
@@ -5,20 +5,21 @@ export function useAnalytics() {
     const gaId = import.meta.env.VITE_GA_ID;
     const isProd = import.meta.env.MODE === "production";
 
-    
     if (!isProd || !gaId) return;
 
     const script = document.createElement("script");
     script.async = true;
     script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args) {
+        window.dataLayer.push(args);
+      }
+
+      gtag("js", new Date());
+      gtag("config", gaId);
+    };
+
     document.head.appendChild(script);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args) {
-      window.dataLayer.push(args);
-    }
-
-    gtag("js", new Date());
-    gtag("config", gaId);
   }, []);
 }
